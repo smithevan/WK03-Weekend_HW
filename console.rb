a8f95a1140a90d8591a10b783e07f25dbb686579
Evan Smith <smith.m.evan@gmail.com>
2019-12-07 15:59:28 +0000

    update method working in all cases including tickets

--- console.rb
+++ console.rb
@@ -37,12 +37,12 @@ film2.save()
 
 ticket1 = Ticket.new({
   "customer_id" => customer1.id,
-  "film_id" => film1.id
+  "films_id" => film1.id
   })
 
 ticket2 = Ticket.new({
   "customer_id" => customer2.id,
-  "film_id" => film2.id
+  "films_id" => film2.id
   })
 
 ticket1.save()
--- models/ticket.rb
+++ models/ticket.rb
@@ -2,12 +2,13 @@ require_relative("../db/sql_runner")
 
 class Ticket
 
-  attr_reader :id, :customer_id, :films_id
+  attr_reader :id
+  attr_accessor :customer_id, :films_id
 
   def initialize(options)
     @id = options["id"].to_i if options["id"]
-    @customer_id = options["customer_id"]
-    @films_id = options["film_id"]
+    @customer_id = options["customer_id"].to_i
+    @films_id = options["films_id"].to_i
   end
 
   def save()
@@ -31,6 +32,12 @@ class Ticket
     sql = "SELECT * FROM tickets"
     results = SqlRunner.run(sql)
     return results.map { |ticket_hash| Ticket.new(ticket_hash)}
+  end
+
+  def update()
+    sql = "UPDATE tickets SET (customer_id, films_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @films_id, @id]
+    SqlRunner.run(sql, values)
   end
 
 end
